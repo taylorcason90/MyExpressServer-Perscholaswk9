f20b3e2fe22d06852510bba7928bfe6e96236919
taylorcason90 <taylorcason90@gmail.com>
2024-04-06 14:09:36 -0400

    server is running, at PORT to .env and app.ts (main app file)

--- newExprSvr-wk10/src/app.ts
+++ newExprSvr-wk10/src/app.ts
@@ -1,10 +1,11 @@
 import express from 'express';
+import config from './config';
 
 async function startServer() {
     const app = express()
 
 
-app.listen(4000, () => console.log(`server is runnging on port 4000`))
+app.listen(config.port, () => console.log(`server is runnging on port ${config.port}`))
 .on('error', error => {
     console.log(error.message);
     process.exit(1);  // will stop the error quickly 
---
+++ newExprSvr-wk10/src/config/index.ts
@@ -0,0 +1,14 @@
+import dotenv from 'dotenv';
+
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'development'
+
+const envFound = dotenv.config();
+
+if (envFound.error) {
+    throw new Error('no .env file found');
+}
+export default {
+    port: parseInt(process.env.PORT, 10) //10 will make sure its parsed
+
+}
