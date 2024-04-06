2037bf68d2f99854ca60f0eabf67ff6724a10268
taylorcason90 <taylorcason90@gmail.com>
2024-04-06 13:54:34 -0400

    server is running on port 4000 in newExprSvr-w10 with npm run dev

--- newExprSvr-wk10/src/app.ts
+++ newExprSvr-wk10/src/app.ts
@@ -1,7 +1,14 @@
 import express from 'express';
 
 async function startServer() {
-    console.log('test')
+    const app = express()
+
+
+app.listen(4000, () => console.log(`server is runnging on port 4000`))
+.on('error', error => {
+    console.log(error.message);
+    process.exit(1);  // will stop the error quickly 
+})
 }
 
 startServer(); 
